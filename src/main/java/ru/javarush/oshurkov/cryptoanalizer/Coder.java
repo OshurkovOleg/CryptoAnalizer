40fb53614a14c2a1184d0026b4746b99093a395c
Oshurkov Oleg <wirc2020@gmail.com>
2022-03-21 14:58:15 +0500

    add brute force method

--- src/main/java/ru/javarush/oshurkov/cryptoanalizer/Coder.java
+++ src/main/java/ru/javarush/oshurkov/cryptoanalizer/Coder.java
@@ -16,6 +16,8 @@ public class Coder {
             'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
             ':', '!', '?', ' ');
 
+    private static final List<String> DICTIONARY = Arrays.asList("но ", "если ", "то ", " к ", " по ", " и ", " пр");
+
     public static final String FILE_NOT_FOUND = "Файл не существует, повторите попытку \n";
     public static final String NOT_FOUND_KEY = "Ключ выходит за пределы допустимых значений, повторите попытку \n";
     public static final String NOT_FOUND_NUMBER = "Введено не число, укажите число в уканных пределах \n";
@@ -23,19 +25,29 @@ public class Coder {
     static Path pathToEncodingFile;
     static int key = 0;
     static char codeChar = 0;
+    static char uncodeChar = 0;
     static Scanner scannerConsole = new Scanner(System.in);
+    static int hackKey = 0;
+    public static int count = 0;
+    public static StringBuilder str = new StringBuilder();
+    public static List<Character> listCharText = new ArrayList<>();
+
 
     public static void main(String[] args) {
 
-/*
-        System.out.println("Доброе пожаловать в программу для шифрования информации.");
+        System.out.println("Доброе пожаловать в программу для кодирования текста.");
         System.out.println("========================================================");
         System.out.println();
 
+        bruteForce();
 
-//        Указываем пути к файлам и проверяем их на наличие файла по указанному пути
+
+    }
+
+
+    static public void codingText() {
         while (true) {
-            System.out.print("Укажите путь до исполняемого файла: ");
+            System.out.print("Укажите путь до файла который будем кодировать: ");
             pathToCodingFile = Path.of(scannerConsole.nextLine());
             if (!Files.isRegularFile(pathToCodingFile)) {
                 System.out.println(FILE_NOT_FOUND);
@@ -45,7 +57,7 @@ public class Coder {
         }
 
         while (true) {
-            System.out.print("Укажите путь до файла с результатом выполнения: ");
+            System.out.print("Укажите путь и название файла куда будем сохранять результат : ");
             pathToEncodingFile = Path.of(scannerConsole.nextLine());
             if (!Files.isRegularFile(pathToEncodingFile)) {
                 System.out.println(FILE_NOT_FOUND);
@@ -54,10 +66,9 @@ public class Coder {
             }
         }
 
-        // Задаем ключ шифрования в пределах от 1 до 127
         do {
             scannerConsole.nextLine();
-            System.out.print("Укажите ключ шифрования от 0 до " + (ALPHABET.size() - 1) + " : ");
+            System.out.print("Укажите ключ кодировки текста от 0 до " + (ALPHABET.size() - 1) + " : ");
 
             if (scannerConsole.hasNextInt()) {
                 key = scannerConsole.nextInt();
@@ -73,13 +84,9 @@ public class Coder {
             }
 
         } while (true);
-*/
 
 
-//**************************************************************************************************
-        // Кодируем текст
-
-/*        try (BufferedReader readeOfCodingFile = newBufferedReader(pathToCodingFile);
+        try (BufferedReader readeOfCodingFile = newBufferedReader(pathToCodingFile);
              BufferedWriter writeOfEncodingFile = newBufferedWriter(pathToEncodingFile)) {
 
 
@@ -87,19 +94,56 @@ public class Coder {
                 char a = Character.toLowerCase((char) readeOfCodingFile.read());
 
                 if (ALPHABET.contains(a)) {
-                        codeChar = ALPHABET.get((ALPHABET.indexOf(a) + key) % ALPHABET.size());
-                        writeOfEncodingFile.write(codeChar);
+                    codeChar = ALPHABET.get((ALPHABET.indexOf(a) + key) % ALPHABET.size());
+                    writeOfEncodingFile.write(codeChar);
                 }
             }
         } catch (IOException e) {
             throw new ExceptionProject("Проблема > " + e);
-        }*/
+        }
+    }
 
 
-//*****************************************************************************************************
-        // Расшифровать текст используя ключ
+    static public void uncodingText() {
+        while (true) {
+            System.out.print("Укажите путь до файла который будем раскодировать: ");
+            pathToCodingFile = Path.of(scannerConsole.nextLine());
+            if (!Files.isRegularFile(pathToCodingFile)) {
+                System.out.println(FILE_NOT_FOUND);
+            } else {
+                break;
+            }
+        }
 
-/*
+        while (true) {
+            System.out.print("Укажите путь и название файла куда будем сохранять результат : ");
+            pathToEncodingFile = Path.of(scannerConsole.nextLine());
+            if (!Files.isRegularFile(pathToEncodingFile)) {
+                System.out.println(FILE_NOT_FOUND);
+            } else {
+                break;
+            }
+        }
+
+        do {
+            scannerConsole.nextLine();
+            System.out.print("Укажите ключ для раскодирования текста : ");
+
+            if (scannerConsole.hasNextInt()) {
+                key = scannerConsole.nextInt();
+
+                if (key < 0 || key > (ALPHABET.size() - 1)) {
+                    System.out.println(NOT_FOUND_KEY);
+                } else {
+                    break;
+                }
+
+            } else {
+                System.out.println(NOT_FOUND_NUMBER);
+            }
+
+        } while (true);
+
         try (BufferedReader readeOfCodingFile = newBufferedReader(pathToCodingFile);
              BufferedWriter writeOfEncodingFile = newBufferedWriter(pathToEncodingFile)) {
 
@@ -107,43 +151,84 @@ public class Coder {
 
             while (readeOfCodingFile.ready()) {
 
-                char b = Character.toLowerCase((char) readeOfCodingFile.read());
+                char symbol = Character.toLowerCase((char) readeOfCodingFile.read());
 
-                if (ALPHABET.contains(b)) {
-                        codeChar = ALPHABET.get((ALPHABET.indexOf(b) + key) % ALPHABET.size());
-                        writeOfEncodingFile.write(codeChar);
+                if (ALPHABET.contains(symbol)) {
+                    codeChar = ALPHABET.get((ALPHABET.indexOf(symbol) + key) % ALPHABET.size());
+                    writeOfEncodingFile.write(codeChar);
                 }
             }
         } catch (IOException e) {
             throw new ExceptionProject("Проблема > " + e);
         }
-*/
+    }
 
 
-//***********************************************************************************************************
-        // Взлом закодиранного текста с перебором ключа
+    static public void bruteForce() {
 
+        while (true) {
+            System.out.print("Укажите путь до файла который будем взламывать: ");
+            pathToCodingFile = Path.of(scannerConsole.nextLine());
+            if (!Files.isRegularFile(pathToCodingFile)) {
+                System.out.println(FILE_NOT_FOUND);
+            } else {
+                break;
+            }
+        }
 
-/*        try (BufferedReader readeOfCodingFile = newBufferedReader(Path.of("C:\\test\\file.txt"));
-             BufferedWriter writeOfEncodingFile = newBufferedWriter(Path.of("C:\\test\\result.txt"))) {
+        while (true) {
+            System.out.print("Укажите путь и название файла куда будем сохранять результат взлома : ");
+            pathToEncodingFile = Path.of(scannerConsole.nextLine());
+            if (!Files.isRegularFile(pathToEncodingFile)) {
+                System.out.println(FILE_NOT_FOUND);
+            } else {
+                break;
+            }
+        }
 
-            int hackKey = 0;
+        try (BufferedReader readeOfCodingFile = newBufferedReader(pathToCodingFile);
+             BufferedWriter writeOfEncodingFile = newBufferedWriter(pathToEncodingFile)) {
+
             Collections.reverse(ALPHABET);
 
             while (readeOfCodingFile.ready()) {
+                char symbol = Character.toLowerCase((char) readeOfCodingFile.read());
+                if (ALPHABET.contains(symbol)) {
+                    listCharText.add(symbol);
+                }
+            }
 
-                char b = Character.toLowerCase((char) readeOfCodingFile.read());
+            while (true) {
 
-                if (ALPHABET.contains(b)) {
-                        codeChar = ALPHABET.get((ALPHABET.indexOf(b) + hackKey) % ALPHABET.size());
-                        writeOfEncodingFile.write(codeChar);
+                for (Character element : listCharText) {
+                    uncodeChar = ALPHABET.get((ALPHABET.indexOf(element) + hackKey) % ALPHABET.size());
+                    str.append(uncodeChar);
+                }
+
+                String strRes = str.toString();
+
+                for (String s : DICTIONARY) {
+                    if (strRes.contains(s)) {
+                        count++;
+                    }
+                }
 
 
+                if (count > 3) {
+                    writeOfEncodingFile.write(strRes);
+                    break;
+                } else {
+                    if (hackKey < ALPHABET.size()) {
+                        hackKey++;
+                        str = new StringBuilder();
+                    } else {
+                        break;
+                    }
                 }
             }
         } catch (IOException e) {
             throw new ExceptionProject("Проблема > " + e);
-        }*/
+        }
 
     }
 }
